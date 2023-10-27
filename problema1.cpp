75ebf5416fd6f0e32f096e3396ec183226984b68
Mochytk <116850132+Mochytk@users.noreply.github.com>
2023-10-27 17:10:42 -0300

    Update problema1.cpp

--- problema1.cpp
+++ problema1.cpp
@@ -1,1 +1,18 @@
 #include <iostream>
+#iclude <fstream>
+#include "LinkList.cpp"
+
+
+class DeBlock {
+  private:
+    int cant_elems;
+    tListadeLista l;
+  public:
+    DeBlock(tElem* elems, int n, int b);
+    int insert(int pos, tElem elem);
+    tElem erase(int pos);
+    tElem get_value(int pos);
+    int length();
+    void clear();
+}
+
