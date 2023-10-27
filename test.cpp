5e1d11ba90eeab2b3d072e858ac10d5ef9bf0258
Mochytk <116850132+Mochytk@users.noreply.github.com>
2023-10-27 17:31:05 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,3 +2,16 @@
 #include <fstream>
 #include "LinkList.cpp"
 #include "DeBlock.cpp"
+
+using namespace std;
+
+int main(){
+  ifstream archivo;
+  archivo.open("pruebas.txt");
+
+  // Verificamos que el archivo abra correctamente
+  if (!archivo.is_open()){
+    cout << "No se pudo abrir el archivo" << endl;
+  }
+  
+}
