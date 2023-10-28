a9b2b5a82515cc0488d79cde4c6875faf785d28d
Sergio Cárcamo <sergio.carcamon@usm.cl>
2023-10-28 19:25:02 -0300

    Constructor de la clase DeBlock (WIP)

--- DeBlock.cpp
+++ DeBlock.cpp
@@ -1,8 +1,11 @@
+#include "LinkList.cpp"
+typedef int tElem;
 
 class DeBlock {
   private:
     int cant_elems;
-    tListadeLista l;
+    LinkList l;
+    LinkList bloqueActual;
   
   public:
     DeBlock(tElem* elems, int n, int b);
@@ -11,11 +14,25 @@ class DeBlock {
     tElem get_value(int pos);
     int length();
     void clear();
-}
+};
 
+DeBlock::DeBlock(tElem* elems, int n, int b) {
+  int numBlocks = (n + b - 1) / b; // Calcula la cantidad de bloques necesarios
+  int indiceActual = 0;
 
-DeBlock::DeBlock(tElem* elems, int n, int b){
-  
+  for (int i = 0; i < numBlocks; i++) {
+    LinkList bloque; // Crea una nueva lista enlazada para cada bloque
+
+    // Llena el bloque con elementos desde el arreglo 'elems'
+    for (int j = 0; j < b && indiceActual < n; j++) {
+      bloque.append(elems[indiceActual]);
+      indiceActual++;
+    }
+
+    l.append(bloque); // Agrega el bloque a la lista de bloques
+  }
+
+  cant_elems = n; // Establece la cantidad total de elementos en 'cant_elems'
 }
 
 int DeBlock::insert(int pos, tElem elem){
