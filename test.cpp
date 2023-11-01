a219acb4ef700d4f1b75d6eba5cfc876f9f9ddfc
Mochytk <116850132+Mochytk@users.noreply.github.com>
2023-10-31 23:23:47 -0300

    Update test.cpp
    Añadi un for para las operaciones, falta desarrollarlo

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "LinkList.cpp"
 #include "DeBlock.cpp"
 
@@ -14,6 +15,27 @@ int main(){
     cout << "No se pudo abrir el archivo" << endl;
   }
   int tam, sub_tam, cant_oper; // creamos variables para el tamaño de la lista, el tamaño de las sub-listas y la cantidad de operaciones a realizar
+  archivo >> tam >> sub_tam; // Lee el tamaño original de la lista y el tamaño de las sub listas
 
+  int lista[tam] // Generamos una arreglo con el tamaño de la lista dada por el archivo
+  for (int i = 0; i < n; i++) {
+    archivo >> lista[i];
+  }
+
+  archivo >> cant_oper; // Lee la cantidad de operaciones
+
+  string operaciones; // Creamos un string para leer las operaciones
+
+  for (int j = 0; j < cant_oper; j++){
+    getline(archivo, operaciones); // Leemos las operaciones una a una
+    string operation;
+    if (operation == "I"){
+      
+      n++;
+    } else if (operation == "G"){
+    } else if (operation == "L"){
+      cout << n << endl;
+    }
+  }
   
 }
