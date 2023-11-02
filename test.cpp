9e13327f4458d45adea2b6114fbcfb96d9c9e3d5
Mochytk <116850132+Mochytk@users.noreply.github.com>
2023-11-02 20:00:41 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,33 +5,42 @@
 #include "DeBlock.cpp"  // Incluye la implementación del TDA DeBlock
 
 int main() {
+    //lee el archivo de entrada
     std::ifstream inputFile("pruebas.txt");
-    if (!inputFile) {
+    
+    if (!inputFile) {        //si no puede abrirlo retorna uno
         std::cerr << "No se pudo abrir el archivo de entrada." << std::endl;
         return 1;
     }
-
+    //Se define el tamaño original de la lista y el tamaño de los bloques
     int tam_ori, tam_blo;
+    
+    //asigna los valores del archivo a las variables
     inputFile >> tam_ori >> tam_blo;
 
     tElem* elems = new tElem[tam_ori];
-    for (int i = 0; i < tam_ori; i++) {
+    for (int i = 0; i < tam_ori; i++) {        //crea un arreglo para inicializar el TDA DeBlock
         inputFile >> elems[i];
     }
-
+    
+    //inicializa el TDA
     DeBlock db(elems, tam_ori, tam_blo);
 
     int cant_op;
     inputFile >> cant_op;
-
+    //numero de operaciones que hará el archivo de entrada
+    
     std::string line;
     std::getline(inputFile, line); // Leer línea en blanco después de q
+    
     for (int i = 0; i < cant_op; i++) {
         std::getline(inputFile, linea);
-        std::istringstream iss(linea);
+        std::istringstream iss(linea);    //lee una linea y la guarda
+    
         std::string operation;
         iss >> operation;
-        
+        //Lee la primera palabra de la línea, que se asume que es la operación a realizar (G, I o L) 
+
         if (operation == "G") {
             int pos;
             iss >> pos;
@@ -56,5 +65,5 @@ int main() {
     delete[] elems;
     inputFile.close();
 
-    return 0;
+    return 0;
 }
